0d2acafda18cab3648b823254e59e7391752abaf
NicoB-UOL <nico.b.nutzer@gmail.com>
2018-01-13 16:26:54 +0100

    updated find_info to reduce redundancy

--- R/find_info.R
+++ R/find_info.R
@@ -10,7 +10,7 @@
 #'     Default is set to 0.
 #'
 #' @return Returns a dataframe with additional project variables, such as number of
-#'      projects, name of the projects and their corresponding IDs
+#'      projects, name of the projects and their corresponding IDs.
 #'
 #' @examples  find_info(id)
 #' @examples
@@ -22,31 +22,59 @@
 #' @export
 
 find_info <- function(x, reqtime = 0) {
-    Sys.sleep(reqtime)
-    id <- as.numeric(x)
-    link <- paste0("http://gepris.dfg.de/gepris/person/", id)
-    page <- httr::GET(link)
-    if(page$status_code < 300){
-        page <- xml2::read_html(page)
-        name <- rvest::html_node(page, "h3")
-        name <- rvest::html_text(name, trim = TRUE)
-        projectlist <- rvest::html_nodes(page, "#projekteNachRolle .intern")
-        anzahl_projekte <- length(projectlist)
-        affiliation <- rvest::html_nodes(page, ".details p:nth-child(1)")
-        affiliation <- rvest::html_text(affiliation, trim = T)
-        project_link <- rvest::html_attr(projectlist, "href")
-        projects <- rvest::html_text(projectlist, trim = TRUE)
-        project_id <- stringr::str_extract(project_link, "\\d+")
+    if(file.exists("temp.rds")){
+
+        temp <- readRDS("temp.rds")
+        temp <- append(temp, x)
+        saveRDS(temp, "temp.rds")
+
+
     } else {
-        projectlist <- list()
-    }
-    if(length(projectlist) == 0) {
-        warning("not a valide ID")
-        data.frame(id = x, name = NA, projects = NA, project_id = NA, anzahl_projekte = NA,
-                   affiliation = NA, stringsAsFactors = F)
-    } else {
-        data.frame(id, name, projects, project_id, anzahl_projekte, affiliation,
-                   stringsAsFactors = F)
+
+        temp <- vector()
+        temp <- x
+        saveRDS(temp,"temp.rds")
+        message("Generating temp.rds in working directory to avoid redundancy. Delete after use with > unlink('temp.rds', recursive=TRUE)")
+
     }
 
+    temp <- readRDS("temp.rds")
+    dutemp <- duplicated(temp)
+    dutemp <- tail(dutemp, 1)
+
+    if(dutemp == FALSE){
+        Sys.sleep(reqtime)
+        id <- as.numeric(x)
+        link <- paste0("http://gepris.dfg.de/gepris/person/", id)
+        page <- httr::GET(link)
+        if (page$status_code < 300) {
+            page <- xml2::read_html(page)
+            name <- rvest::html_node(page, "h3")
+            name <- rvest::html_text(name, trim = TRUE)
+            projectlist <- rvest::html_nodes(page, "#projekteNachRolle .intern")
+            anzahl_projekte <- length(projectlist)
+            affiliation <- rvest::html_nodes(page, ".details p:nth-child(1)")
+            affiliation <- rvest::html_text(affiliation, trim = T)
+            project_link <- rvest::html_attr(projectlist, "href")
+            projects <- rvest::html_text(projectlist, trim = TRUE)
+            project_id <- stringr::str_extract(project_link, "\\d+")
+
+        }
+        else {
+            projectlist <- list()
+        }
+        if (length(projectlist) == 0) {
+            warning("not a valide ID")
+            data.frame(id = x, name = NA, projects = NA, project_id = NA,
+                       anzahl_projekte = NA, affiliation = NA, stringsAsFactors = F)
+        }
+        else {
+            data.frame(id, name, projects, project_id, anzahl_projekte,
+                       affiliation, stringsAsFactors = F)
+        }
+    } else {
+        message("Duplicated ID detected proceeding with next ID.")
+        data.frame(id = NA, name = NA, projects = NA, project_id = NA,
+                   anzahl_projekte = NA, affiliation = NA, stringsAsFactors = F)
+    }
 }
